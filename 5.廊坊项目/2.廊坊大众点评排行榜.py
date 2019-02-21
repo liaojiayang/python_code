fb31a79ea2680586e559362d4c2fe58d083e8a4e
woody woody <44455704+liaojiayang@users.noreply.github.com>
2019-02-21 15:29:48 +0800

    rank id为城市名称

--- 5.廊坊项目/2.廊坊大众点评排行榜.py
+++ 5.廊坊项目/2.廊坊大众点评排行榜.py
@@ -1,10 +1,9 @@
 import requests,bs4,time,json
 
-w = open('H:/OneDrive/工作/编程设计/spyder/5.廊坊项目/dianpin.csv','w',encoding='utf-8')
-url_1 = 'http://www.dianping.com/shoplist/shopRank/pcChannelRankingV2?rankId=7acb1683f1cc89810888f620c792da28' #廊坊为城市名称
+w = open('H:/OneDrive/工作/编程设计/spyder/5.廊坊项目/dianpin.txt','w',encoding='utf-8')
+url_1 = 'http://www.dianping.com/shoplist/shopRank/pcChannelRankingV2?rankId=7acb1683f1cc89810888f620c792da28' # rankId为城市名称
 headers = {'Cookie': 'td_cookie=2464111219; _lxsdk_cuid=1690e07db3dc8-00119572b83533-57b1a3f-1fa400-1690e07db3ec8; _lxsdk=1690e07db3dc8-00119572b83533-57b1a3f-1fa400-1690e07db3ec8; _hc.v=93b7e7fa-41e7-eaf4-350f-db49d1812089.1550718590; cy=33; cye=langfang; _lxsdk_s=1690e07db3e-487-31e-98f%7C%7C324'
 ,'Host': 'www.dianping.com'
-,'Referer': 'http://www.dianping.com/langfang'
 ,'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.64 Safari/537.36'
 ,'X-Requested-With': 'XMLHttpRequest'
            }
